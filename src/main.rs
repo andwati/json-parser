3106445f95c0e2f15b7ee167bbe9fd73e968e295
Ian Andwati <andwatiian@gmail.com>
2024-10-30 15:51:20 +0300

    created tokenize function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use std::env;
-use std::process::exit;
 use std::fs;
+use std::process::exit;
 
+#[derive(Debug)]
+enum Token {
+    CurlyOpen,
+    CurlyClose,
+    Colon,
+    Comma,
+    String(String),
+}
 fn main() {
-   let args: Vec<String> = env::args().collect();
-    if args.len() < 2{
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
         eprintln!("Usage: json_parser <file_path>");
         exit(1);
     }
@@ -15,7 +23,7 @@ fn main() {
             if is_valid_json(&content) {
                 println!("Valid JSON");
                 exit(0);
-            }else{
+            } else {
                 eprintln!("Invalid JSON");
                 exit(1);
             }
@@ -26,9 +34,39 @@ fn main() {
             exit(1);
         }
     }
-
 }
 
-fn is_valid_json(content: &str)-> bool{
+fn is_valid_json(content: &str) -> bool {
     content.trim() == "{}"
+}
+
+fn tokenize(content: &str) -> Result<Vec<Token>, String> {
+    let mut tokens = Vec::new();
+    let chars: Vec<char> = content.chars().collect();
+    let mut i = 0;
+
+    while i < chars.len() {
+        match chars[i] {
+            '{' => tokens.push(Token::CurlyOpen),
+            '}' => tokens.push(Token::CurlyClose),
+            ':' => tokens.push(Token::Colon),
+            ',' => tokens.push(Token::Comma),
+            '"' => {
+                let mut j = i + 1;
+                while j < chars.len() && chars[j] != '"' {
+                    j += 1;
+                }
+                if j >= chars.len() {
+                    return Err("Unclose string".into());
+                }
+                let string_content: String = chars[i + 1..j].iter().collect();
+                tokens.push(Token::String(string_content));
+                i = j;
+            }
+            c if c.is_whitespace() => {}
+            _ => return Err(format!("Unexpected character '{}'", chars[i])),
+        }
+        i += 1;
+    }
+    Ok(tokens)
 }
